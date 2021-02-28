0dc566ce0d8ba909c5b8214d8d9870875c5a6e74
gHernandez2410 <gonzalo.hernandez2410@gmail.com>
2021-02-28 20:03:24 +0200

    ADD: auth with JWT

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
 
 const app = express();
 dotenv.config();
@@ -14,6 +16,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 app.get('/', (req, res) => {
   res.send('Wellcome to my memories API')
--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,14 @@
 import express from 'express';
+
 import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
 router.get('/', getPosts);
-router.post('/', createPost);
-router.get('/:id', getPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/likePost/:id', likePost);
-
+router.post('/',auth,  createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/likePost/:id', auth, likePost);
 
 export default router;
